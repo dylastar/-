4c15dcc14082382cd5203f8611ba6ea2d876aa8b
Dylan <2405893634@qq.com>
2023-05-29 01:16:59 +0800

    Layout的Nav/Footer/Head完成

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
  
- 
 
 </script>
 
 <template>
-   <!-- <el-button type="primary">Primary</el-button> -->
+  
 <RouterView />
-<div class="test">
-  test
-</div>
+
+
 
 </template>
 
 <style scoped lang="scss">
-.test {
-  color: $priceColor;
-}
+
 </style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,3 +1,13 @@
+<script setup>
+
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    我是首页
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
